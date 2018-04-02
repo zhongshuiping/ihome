2e3f6ff88ddca7bd67ccb9bb0646edbf905212ae
zsp123456 <zhongshuiping>
2018-04-02 09:35:05 +0800

    增加了logging的日志,使用current_app的日志,因为他比原生的更好

--- ihome/api_1_0/verify.py
+++ ihome/api_1_0/verify.py
@@ -1,18 +1,23 @@
 from . import api
 from ihome.utils.captcha.captcha import captcha
-from flask import make_response,request
+from flask import make_response, request, current_app
 from config import Config
 
 redis = Config.get_redis_connect()
+
 
 @api.route('/verify_code')
 def verify():
     name, text, image = captcha.generate_captcha()
     uuid = request.args.get('uuid')
     last_uuid = request.args.get('last_uuid')
-    if last_uuid:
-        redis.delete(last_uuid)
-    redis.set(uuid,text,ex=60*5)
+    try:
+        if last_uuid:
+            redis.delete(last_uuid)
+        redis.set(uuid, text, ex=60 * 5)
+    except Exception as e:
+        current_app.logger.error(e)
+
     response = make_response(image)
     response.headers['Content-Type'] = 'image/jpg'
     return response
